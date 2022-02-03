493c4b4b730dd178fb72c2953380211b65adcbec
Bazulenkov <anton@bazul.ru>
2022-02-03 19:08:34 +0300

    Исправлен путь до API

--- homework.py
+++ homework.py
@@ -21,7 +21,7 @@ TELEGRAM_TOKEN = os.getenv("telegram_token", "")
 TELEGRAM_CHAT_ID = os.getenv("telegram_chat", "")
 
 RETRY_TIME = 600
-ENDPOINT = "https://practicum.yandex.ru/api/999/user_api/homework_statuses/"
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 HOMEWORK_STATUSES = {
