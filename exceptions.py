e6cbdea1699a4717bf7a905d43448c0c7983a4e5
Bazulenkov <anton@bazul.ru>
2022-11-23 20:44:21 +0400

    feat: update practicum-tests.

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,20 @@ class HomeworkStatusException(Exception):
     """Неверный статус домашней работы."""
 
     pass
+
+
+class EndpointResponseError(Exception):
+    """Ошибка ответа API."""
+
+    def __init__(self, *args):
+        self.response = args[0] if args else None
+
+    def __str__(self):
+        message = (
+            "Ошибка при обращении к API Яндекс.Практикума: ",
+            f'Код ответа: {self.response.get("code")}',
+            f'Сообщение сервера: {self.response.get("message")}',
+        )
+        return message
+
+    pass
--- homework.py
+++ homework.py
@@ -21,11 +21,11 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN", "")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT", "")
 
-RETRY_TIME = 600
+RETRY_PERIOD = 600
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
     "reviewing": "Работа взята на проверку ревьюером.",
     "rejected": "Работа проверена: у ревьюера есть замечания.",
@@ -55,27 +55,29 @@ def send_message(bot: telegram.Bot, message: str):
         raise e
 
 
-def get_api_answer(current_timestamp: int = 0) -> Dict:
+def get_api_answer(timestamp: int = 0) -> Dict:
     """Делает запрос к эндпоинту API-сервиса.
 
     Args:
-        current_timestamp: временная метка в формате Unix time.
+        timestamp: временная метка в формате Unix time.
 
     Returns:
         В случае успешного запроса возвращает ответ API, преобразовав его
     из формата JSON к типам данных Python.
         Либо словарь со списком ошибок.
     """
-    params = {"from_date": current_timestamp}
+    params = {"from_date": timestamp}
 
     try:
         response = requests.get(url=ENDPOINT, params=params, headers=HEADERS)
-    except ConnectionError as e:
+    except requests.RequestException as e:
         logger.error(e)
         raise e
-
     response.raise_for_status()
-
+    # if response.status_code != 200:
+    #     raise ConnectionError(
+    #         f"Response received not success: status_code = {response.status_code}"
+    #     )
     try:
         result = response.json()
     except requests.exceptions.JSONDecodeError as e:
@@ -127,7 +129,7 @@ def parse_status(homework: Dict[str, str]) -> str:
     Returns:
         В случае успеха, функция возвращает подготовленную для отправки в
         Telegram строку, содержащую один из вердиктов
-        словаря HOMEWORK_STATUSES.
+        словаря HOMEWORK_VERDICTS.
     """
     # ошибка в тесте
     # tests/test_bot.py::TestHomework::test_parse_status_no_homework_name_key
@@ -142,11 +144,11 @@ def parse_status(homework: Dict[str, str]) -> str:
         logger.error(e)
         raise e
 
-    if homework_status not in HOMEWORK_STATUSES:
+    if homework_status not in HOMEWORK_VERDICTS:
         logger.error("Получен неизвестный статус домашней работы")
         raise HomeworkStatusException
 
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -178,27 +180,27 @@ def main():
         sys.exit("Interrupt: Missing env variables")
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    timestamp = int(time.time())
 
     # d = datetime.date(2022, 1, 1)
-    # current_timestamp = int(time.mktime(d.timetuple()))
+    # timestamp = int(time.mktime(d.timetuple()))
 
     while True:
         try:
-            response = get_api_answer(current_timestamp)
+            response = get_api_answer(timestamp)
             homeworks: List = check_response(response)
             for hw in homeworks:
                 message = parse_status(hw)
                 send_message(bot, message)
-            current_timestamp = int(response["current_date"])
+            timestamp = int(response["current_date"])
 
         except Exception as error:
             error_message = f" Сбой в работе программы: {error}"
             logger.error(error_message)
             send_message(bot, error_message)
         finally:
-            logger.info(f"Следующая проверка через {RETRY_TIME/60} минут")
-            time.sleep(RETRY_TIME)
+            logger.info(f"Следующая проверка через {RETRY_PERIOD / 60} минут")
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == "__main__":
